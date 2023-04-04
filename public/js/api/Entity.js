209640261e07d15d0f869d9159f597f25f6a2520
Nikita Gudin <nikitagudin.ads@gmail.com>
2023-04-04 22:12:31 +0300

    Entity is ready

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,7 +1,10 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * */
+/*
+Это базовый класс, от которого будут наследоваться классы Account и Transaction. 
+Необходим для организации взаимодействия между интерфейсом программы и сервером через функцию createRequest. 
+Если пользователю необходимо получить, изменить или добавить данные, то происходит обращение к методам данного класса, 
+которые делают запрос к серверу через функцию createRequest.
+*/
+
 class Entity {
   /**
    * Запрашивает с сервера список данных.
@@ -9,8 +12,15 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static list(data, callback){
+    const options = {
+      url: this.URL,
+      callback: callback,
+      method: 'GET',
+      data: {...data},
+    };
 
-  }
+    createRequest(options);
+  };
 
   /**
    * Создаёт счёт или доход/расход с помощью запроса
@@ -18,14 +28,28 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create(data, callback) {
+    const options = {
+      url: this.URL,
+      callback: callback,
+      method: 'PUT',
+      data: {...data},
+    };
 
-  }
+    createRequest(options);
+  };
 
   /**
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove(data, callback ) {
+    const options = {
+      url: this.URL,
+      callback: callback,
+      method: 'DELETE',
+      data: {...data},
+    };
+  };
 
-  }
+  static URL = '';
 }
