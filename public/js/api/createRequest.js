257cb0828de5afd699a6c2819080802d2c10c361
Nikita Gudin <nikitagudin.ads@gmail.com>
2023-04-04 19:58:02 +0300

    createRequest complete

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,45 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  
+  const { url, data, method } = options;
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
 
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+      try {
+        if(xhr.status >= 200 && xhr.status < 300) {
+          options.callback(null, xhr.response);
+        } else {
+          throw new Error(`Request failed with status ${xhr.status}`);
+        };
+      } catch(error) {
+        options.callback(error, null);
+      };
+    };
+  };
+
+
+  if(method === 'GET') {
+    const params = Object.entries(data)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+
+    const fullUrl = url + '?' + params;
+
+    xhr.open(method, fullUrl);
+    xhr.send();
+
+  } else {
+    const formData = new FormData();
+
+    const params = Object.entries(data)
+    .map(([key, value]) => formData.append(key, value));
+
+    xhr.open(method, url);
+    xhr.send(formData);
+  };
+
+  
 };
