6d60125116d0f9414904b1cbc2cd13105f0fe056
Nikita Gudin <nikitagudin.ads@gmail.com>
2023-04-08 18:52:34 +0300

    sidebarToggle complete

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,13 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const bodyMini = document.querySelector('.sidebar-mini');
+    const sidebarToggle = document.querySelector('.sidebar-toggle');
 
+    sidebarToggle.addEventListener('click', (event) => {
+      bodyMini.classList.toggle('sidebar-open');
+      bodyMini.classList.toggle('sidebar-collapse');
+    });
   }
 
   /**
