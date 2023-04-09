43f8ba1fdb1cec9b0c3e2869c5f7b96f06e2e070
Nikita Gudin <nikitagudin.ads@gmail.com>
2023-04-09 17:20:31 +0300

    AsyncForm complete

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -13,7 +13,12 @@ class AsyncForm {
    * через registerEvents()
    * */
   constructor(element) {
+    if(!element) {
+      throw new Error('Не передана форма в конструктор');
+    };
 
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -21,7 +26,10 @@ class AsyncForm {
    * вызывает метод submit()
    * */
   registerEvents() {
-
+    this.element.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.submit();
+    })
   }
 
   /**
@@ -32,7 +40,14 @@ class AsyncForm {
    * }
    * */
   getData() {
+    const data = {};
+    const formData = new FormData(this.element);
+    
+    for(let [name, value] of formData.entries()) {
+      data[name] = value;
+    };
 
+    return data;
   }
 
   onSubmit(options){
@@ -44,6 +59,7 @@ class AsyncForm {
    * данные, полученные из метода getData()
    * */
   submit() {
-
+    const data = this.getData();
+    this.onSubmit(data);
   }
 }
