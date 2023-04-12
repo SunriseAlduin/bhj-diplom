1c10e02cc404125187b45f0892ca3e07ac76df64
Nikita Gudin <nikitagudin.ads@gmail.com>
2023-04-12 17:54:06 +0300

    UserWidget complete

--- public/js/ui/widgets/UserWidget.js
+++ public/js/ui/widgets/UserWidget.js
@@ -12,7 +12,11 @@ class UserWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
+    if(!element) {
+      throw new Error('UserWidget не передан');
+    };
 
+    this.element = element;
   }
 
   /**
@@ -23,6 +27,10 @@ class UserWidget {
    * авторизованного пользователя
    * */
   update(){
-
+    const current = User.current();
+    const userName = document.querySelector('.user-name');
+    if(current) {
+      userName.textContent = current.name;
+    };
   }
 }
